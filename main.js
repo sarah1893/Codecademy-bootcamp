d48ade93ce0cf4ee9a45a915f63907522384c82e
Sarah <salibay@bi-consulting.com>
2020-07-04 11:16:35 +0200

    Dom events in js

---
+++ main.js
@@ -0,0 +1,7 @@
+let readMore = document.getElementById('read-more')
+let moreInfo = document.getElementById('more-info')
+
+// Write your code here:
+readMore.onclick = function() {
+  moreInfo.style.display = 'block';
+}
